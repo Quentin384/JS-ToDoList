f43ac6e6320b74f009fdb38bf46d636915ed5e62
Quentin384 <zampieri.quentin38@gmail.com>
2025-04-09 19:25:00 +0200

    Adding invalid input and responsive

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const listInput = document.getElementById("list-input");
 const button = document.getElementById("submit");
 const listOutput = document.getElementById("todo-list");
-const elementList = document.getElementById("elementList")
+const elementList = document.getElementById("elementList");
 
 /* Creation du squelette de tache */
 
 class Task {
     constructor(title) {
-        this.title = title
+        this.title = title;
     }
 }
 
@@ -17,25 +17,30 @@ class Task {
 button.addEventListener("click", (event) => {
     event.preventDefault();
 
-    const inputValue = listInput.value;
+    const inputValue = listInput.value.trim();
 
- // Creation de l'instance de taches  
+    // Vérifier si l'input est vide
+    if (inputValue === "") {
+        alert("Veuillez entrer une tâche valide !");
+        return;
+    }
 
+    // Creation de l'instance de taches  
     const newTask = new Task(inputValue);
 
- /* OUTPUT 
-    Creation de l'element de liste pour l'instance de taches */    
+    /* OUTPUT 
+       Creation de l'element de liste pour l'instance de taches */    
 
     const taskElement = document.createElement("li");
     taskElement.textContent = newTask.title;
     taskElement.classList.add('delete');
 
- // Creation du bouton pour la suppression
+    // Creation du bouton pour la suppression
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "❌";
 
- // Au clic sur le bouton delete   
+    // Au clic sur le bouton delete   
 
     deleteBtn.addEventListener("click", () => {
         listOutput.removeChild(taskElement);
